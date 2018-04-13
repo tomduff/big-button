67cbae5ca7cc51e3b532c148afc8dbe1215c05fb
Tom Duff <tomduff@users.noreply.github.com>
2018-04-13 21:23:50 +0100

    Some updates... wtf is going on?

--- test/TrackTest.cpp
+++ test/TrackTest.cpp
@@ -7,4 +7,20 @@ unittest(pretend_equal_things_arent)
   assertNotEqual(x, y);
 }
 
+unittest(initialse)
+{
+  Track track = TRack();
+  track.initialise();
+  assertEqual(0, track.getPattern();
+  assertEqual(MAX_STEPS, track.getLength());
+  assertEqual(0, track.getDensity());
+  assertEqual(0, track.getOffset());
+  assertEqual(0, track.getDivider());
+  assertEqual(0, track.getShuffle());
+  assertEqual(PlayMode::Forward, track.getPlayMode()));
+  assertEqual(OutMode::Trigger, track.getOutMode();
+  assertEqual(PatternType::Programmed, track.getPatternType());
+  assertEqual(DividerType::Beat, track.getDividerType());
+}
+
 unittest_main()
