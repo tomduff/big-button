0b0f0bf5548a2f0c2197c698a046880023add642
Tom Duff <tomduff@users.noreply.github.com>
2018-04-14 08:01:22 +0100

    bleh - I should probably have done this one a different branch

--- big-button-test.cpp
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-#include "big-button-test.h"
-int doSomething(void) {
-  millis();  // this line is only here to test that we're able to refer to the builtins
-  return 4;
-};
--- big-button-test.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <Arduino.h>
-int bigButtonTest(void);
