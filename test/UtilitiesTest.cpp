5dc53fc52a7c3fe0707e6843ac9b87650639ed37
Tom Duff <tomduff@users.noreply.github.com>
2018-04-14 21:19:35 +0100

    More examples of why I need to get this compiling the test folder

--- test/UtilitiesTest.cpp
+++ test/UtilitiesTest.cpp
@@ -8,23 +8,23 @@ unittest(bound_int)
 {
     int value = min_bound;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(min_bound, value);
+    assertEqual(min_bound, value);
 
     value = max_bound;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(max_bound, value);
+    assertEqual(max_bound, value);
 
     value = 7;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(7, value);
+    assertEqual(7, value);
 
     value = - 1;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(min_bound, value);
+    assertEqual(min_bound, value);
 
     value = 16;
     Utilities::bound(value, min_bound ,max_boundmax);
-    assertEquals(max_bound, value);
+    assertEqual(max_bound, value);
 }
 
 unittest(bound_byte)
@@ -32,23 +32,23 @@ unittest(bound_byte)
 
     byte value = min_bound;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(min_bound, value);
+    assertEqual(min_bound, value);
 
     value = max_bound;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(max_bound, value);
+    assertEqual(max_bound, value);
 
     value = 4;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(7, value);
+    assertEqual(7, value);
 
     value = -2;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(mmin_bound, value);
+    assertEqual(mmin_bound, value);
 
     value = 20;
     Utilities::bound(value, min_bound, max_bound);
-    assertEquals(max_bound, value);
+    assertEqual(max_bound, value);
 }
 
 unittest_main()
