3dcf38669a88123af6236d4c7470136d4f4c4bad
Tom Duff <tomduff@users.noreply.github.com>
2018-04-14 22:06:07 +0100

    Added a test back in

--- test/UtilitiesTest.cpp
+++ test/UtilitiesTest.cpp
@@ -4,55 +4,51 @@
 #define min_bound 0
 #define max_bound 15
 
-unittest(simple) {
-    assertNotEqual(min_bound,max_bound);
+unittest(bound_int)
+{
+    int value = min_bound;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(min_bound, value);
+
+    value = max_bound;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(max_bound, value);
+
+    value = 7;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(7, value);
+
+    value = -1;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(min_bound, value);
+
+    value = 16;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(max_bound, value);
 }
 
-// unittest(bound_int)
-// {
-//     int value = min_bound;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(min_bound, value);
+unittest(bound_byte)
+{
 
-//     value = max_bound;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(max_bound, value);
+    byte value = min_bound;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(min_bound, value);
 
-//     value = 7;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(7, value);
+    value = max_bound;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(max_bound, value);
 
-//     value = -1;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(min_bound, value);
+    value = 4;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(7, value);
 
-//     value = 16;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(max_bound, value);
-// }
+    value = -2;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(min_bound, value);
 
-// unittest(bound_byte)
-// {
-
-//     byte value = min_bound;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(min_bound, value);
-
-//     value = max_bound;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(max_bound, value);
-
-//     value = 4;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(7, value);
-
-//     value = -2;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(min_bound, value);
-
-//     value = 20;
-//     Utilities::bound(value, min_bound, max_bound);
-//     assertEqual(max_bound, value);
-// }
+    value = 20;
+    Utilities::bound(value, min_bound, max_bound);
+    assertEqual(max_bound, value);
+}
 
 unittest_main()
