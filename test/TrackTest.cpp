bab1bf6fe0c24faf7e08a57615009ac062bbf50d
Tom Duff <tomduff@users.noreply.github.com>
2018-04-15 06:02:59 +0100

    typos

--- test/TrackTest.cpp
+++ test/TrackTest.cpp
@@ -6,14 +6,14 @@ unittest(initialse)
   Track track = Track();
   track.initialise();
   
-  assertEqual(0, track.getPattern();
+  assertEqual(0, track.getPattern());
   assertEqual(MAX_STEPS, track.getLength());
   assertEqual(0, track.getDensity());
   assertEqual(0, track.getOffset());
   assertEqual(0, track.getDivider());
   assertEqual(0, track.getShuffle());
   assertEqual(PlayMode::Forward, track.getPlayMode()));
-  assertEqual(OutMode::Trigger, track.getOutMode();
+  assertEqual(OutMode::Trigger, track.getOutMode());
   assertEqual(PatternType::Programmed, track.getPatternType());
   assertEqual(DividerType::Beat, track.getDividerType());
 }
